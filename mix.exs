32b8782596b2f241470c5a8488fd4b54216e034b
Milton Mazzarri <milmazz@gmail.com>
2017-04-08 02:55:23 -0500

    Release v0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hunter.Mixfile do
 
   def project do
     [app: :hunter,
-     version: "0.2.0-dev",
+     version: "0.2.0",
      elixir: "~> 1.3",
      docs: docs(),
      package: package(),
