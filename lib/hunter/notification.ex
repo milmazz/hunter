ddf464fb72bd36c20598de4ca7eaa78a540e56a6
Milton Mazzarri <milmazz@gmail.com>
2017-04-10 21:44:58 -0500

    New methods to manage notifications

--- lib/hunter/notification.ex
+++ lib/hunter/notification.ex
@@ -14,6 +14,8 @@ defmodule Hunter.Notification do
     * `status` - The `Hunter.Status` associated with the notification, if applicable
 
   """
+  @hunter_api Application.get_env(:hunter, :hunter_api)
+
   @type t :: %__MODULE__{
     id: String.t,
     type: String.t,
@@ -24,4 +26,44 @@ defmodule Hunter.Notification do
 
   @derive [Poison.Encoder]
   defstruct [:id, :type, :created_at, :account, :status]
+
+  @doc """
+  Retrieve user's notifications
+
+  ## Parameters
+
+    * `conn` - connection credentials
+
+  """
+  @spec notifications(Hunter.Client.t) :: [Hunter.Notification.t]
+  def notifications(conn) do
+    @hunter_api.notifications(conn)
+  end
+
+  @doc """
+  Retrieve single notification
+
+  ## Parameters
+
+    * `conn` - connection credentials
+    * `id` - notification identifier
+
+  """
+  @spec notification(Hunter.Client.t, non_neg_integer) :: Hunter.Notification.t
+  def notification(conn, id) do
+    @hunter_api.notification(conn, id)
+  end
+
+  @doc """
+  Deletes all notifications from the Mastodon server for the authenticated user
+
+  ## Parameters
+
+    * `conn` - connection credentials
+
+  """
+  @spec clear_notifications(Hunter.Client.t) :: map
+  def clear_notifications(conn) do
+    @hunter_api.clear_notifications(conn)
+  end
 end
