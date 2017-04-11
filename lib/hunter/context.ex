eb8655970243f25484599528f9a552cc88bda542
Milton Mazzarri <milmazz@gmail.com>
2017-04-10 21:46:55 -0500

    Get status context

--- lib/hunter/context.ex
+++ lib/hunter/context.ex
@@ -8,5 +8,27 @@ defmodule Hunter.Context do
     * `descendants` - The descendants of the status in the conversation, as a list of Statuses
 
   """
+  @hunter_api Application.get_env(:hunter, :hunter_api)
+
+  @type t :: %__MODULE__{
+    ancestors: [Hunter.Status.t],
+    descendants: [Hunter.Status.t]
+  }
+
+  @derive [Poison.Encoder]
   defstruct [:ancestors, :descendants]
+
+  @doc """
+  Retrieve status context
+
+  ## Parameters
+
+    * `conn` - connection credentials
+    * `id` - status identifier
+
+  """
+  @spec status_context(Hunter.Client.t, non_neg_integer) :: Hunter.Context.t
+  def status_context(conn, id) do
+    @hunter_api.status_context(conn, id)
+  end
 end
