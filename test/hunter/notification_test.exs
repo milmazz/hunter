49e56fe52df279511e5b796255719535407b05e8
Milton Mazzarri <milmazz@gmail.com>
2017-04-16 22:40:46 -0500

    Add test for Notifications

---
+++ test/hunter/notification_test.exs
@@ -0,0 +1,21 @@
+defmodule Hunter.NotificationTest do
+  use ExUnit.Case, async: true
+
+  alias Hunter.Notification
+
+  setup do
+    [conn: Hunter.Client.new([base_url: "https://example.com", bearer_token: "123456"])]
+  end
+
+  test "fetch user's notifications", %{conn: conn} do
+    notifications = conn |> Notification.notifications() |> List.first()
+    assert "paperswelove" == notifications.account["username"]
+    assert "follow" == notifications.type
+  end
+
+  test "fetch a single notification", %{conn: conn} do
+    notification = Notification.notification(conn, 17476)
+    assert "paperswelove" == notification.account["username"]
+    assert "follow" == notification.type
+  end
+end
