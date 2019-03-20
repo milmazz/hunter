3473d3356637c0c2e4dccf0327bc35ab7b0d21d5
Milton Mazzarri <milmazz@gmail.com>
2019-03-19 23:06:26 -0500

    Add doctests to Hunter.Config

--- lib/hunter/config.ex
+++ lib/hunter/config.ex
@@ -3,18 +3,52 @@ defmodule Hunter.Config do
   Hunter configuration.
   """
 
+  @doc """
+  Returns adapter module to do run API calls.
+
+  ## Examples
+
+      iex> Hunter.Config.hunter_api()
+      Hunter.ApiMock
+
+  """
   def hunter_api do
     Application.get_env(:hunter, :hunter_api, Hunter.Api.HTTPClient)
   end
 
+  @doc """
+  Returns the API base URL
+
+  ## Examples
+
+      iex> Hunter.Config.api_base_url()
+      "https://mastodon.social"
+
+  """
   def api_base_url do
     Application.get_env(:hunter, :api_base_url, "https://mastodon.social")
   end
 
+  @doc """
+  Returns the Hunter home directory
+
+  ## Examples
+
+      iex> Path.extname(Hunter.Config.home())
+      ".hunter"
+
+  """
   def home do
     Path.expand(System.get_env("HUNTER_HOME") || "~/.hunter")
   end
 
+  @doc """
+  Returns HTTP options
+
+      iex> Hunter.Config.http_options()
+      []
+
+  """
   def http_options do
     Application.get_env(:hunter, :http_options, [])
   end
---
+++ test/hunter_test.exs
@@ -0,0 +1,5 @@
+defmodule HunterTest do
+  use ExUnit.Case, async: true
+
+  doctest Hunter.Config
+end
