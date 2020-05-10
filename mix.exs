7cd99a946a855848df18776fd73008b9f1e0185e
Milton Mazzarri <milmazz@gmail.com>
2020-05-10 02:57:24 -0500

    finish initial setup for GitHub Actions (#43)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hunter.Mixfile do
     [
       app: :hunter,
       version: "0.5.1",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       docs: docs(),
       package: package(),
       source_url: "https://github.com/milmazz/hunter",
